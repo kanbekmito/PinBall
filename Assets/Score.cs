70fe30eb3c6a45a4b35c18ebdc0f7f2979a03567
hideyuki arakawa <hideyuki-kreva@hotmail.co.jp>
2017-01-24 22:01:35 +0900

    PinBall
    Lesson5課題再提出

---
+++ Assets/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private GameObject scoreText;
+    void Start()
+    {
+        this.scoreText = GameObject.Find("GameScoreText");
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (tag == "SmallStarTag")
+        {
+            this.scoreText.GetComponent<ScoreTex>().score += 5;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.scoreText.GetComponent<ScoreTex>().score += 10;
+        }
+        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.scoreText.GetComponent<ScoreTex>().score += 20;
+        }
+    }
+}
---
+++ Assets/ScoreTex.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTex : MonoBehaviour {
+
+    public int score = 0;
+    private GameObject scoreText;
+
+    // Use this for initialization
+    void Start () {
+        this.scoreText = GameObject.Find("GameScoreText");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.GetComponent<Text>().text = score.ToString();
+    }
+}
